9cb1fb8c41aafe3eb23dd8c614bb0ce8a2af4f21
Ibrahim Kalaileh <ibrahim.al.kalaileh@gmail.com>
2023-02-12 01:39:25 +0300

    added project card and tabs with animation and hover effects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,10 @@
 import React from 'react'
-
+import {Col,Container,Tab,Row,Nav} from 'react-bootstrap';
+import ProjectCard from './ProjectCard';
+import colorBG from "../assets/img/color-sharp2.png"
+import projImg1 from "../assets/img/project-img1.png"
+import projImg2 from "../assets/img/project-img2.png"
+import projImg3 from "../assets/img/project-img3.png"
 function projects() {
   const projects =[
     {
@@ -36,11 +41,13 @@ function projects() {
 
   return (
     <section className='project' id='project'>
+    <Container>
       <Row>
         <Col>
-          <h2>projects</h2>
+          <h2>Projects</h2>
           <p> Lorem Ipsum</p>
-          <Nav variant="pills" defaultActiveKey="/home">
+          <Tab.Container id='projects-tabs' defaultActiveKey='first'>
+          <Nav variant="pills" className='nav-pills mb-5 justify-content-center align-items-center' id='pills-tab'>
             <Nav.Item>
               <Nav.Link eventKey='first'>Tab One</Nav.Link>
             </Nav.Item>
@@ -53,8 +60,63 @@ function projects() {
               </Nav.Link>
             </Nav.Item>
           </Nav>
+          <Tab.Content>
+            
+            <Tab.Pane eventKey='first'>
+              <Row>
+                {
+                  projects.map((project,index)=>{
+                    return(
+                      <ProjectCard
+                        key={index}
+                        {...project}
+                       />
+                    )
+                  })
+                }
+              </Row>
+            </Tab.Pane>
+
+            <Tab.Pane eventKey='second' >
+
+                <Row>
+                  {
+                    projects.map((project,index)=>{
+                      return(
+                        <ProjectCard
+                          key={index}
+                          {...project}
+                        />
+                      )
+                    })
+                  }
+                </Row>
+                
+            </Tab.Pane>
+
+            <Tab.Pane eventKey='third'>
+
+                <Row>
+                  {
+                    projects.map((project,index)=>{
+                      return(
+                        <ProjectCard
+                          key={index}
+                          {...project}
+                        />
+                      )
+                    })
+                  }
+                </Row>
+
+            </Tab.Pane>
+
+          </Tab.Content>
+          </Tab.Container>
         </Col>
       </Row>
+    </Container>
+    <img className='background-image-right' src={colorBG}/>
     </section>
   )
 }
