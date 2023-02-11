d672b03bd61216587ded614dd64530ad11576bbf
Ibrahim Kalaileh <ibrahim.al.kalaileh@gmail.com>
2023-02-12 01:02:11 +0300

    projects tabs

--- src/components/projects.js
+++ src/components/projects.js
@@ -32,10 +32,30 @@ function projects() {
         description: "Design & Development",
         imgUrl: projImg3,
       }
-  ]
+  ];
 
   return (
-    
+    <section className='project' id='project'>
+      <Row>
+        <Col>
+          <h2>projects</h2>
+          <p> Lorem Ipsum</p>
+          <Nav variant="pills" defaultActiveKey="/home">
+            <Nav.Item>
+              <Nav.Link eventKey='first'>Tab One</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="second">Tab 2</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="third">
+                Tab 3
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+      </Row>
+    </section>
   )
 }
 
