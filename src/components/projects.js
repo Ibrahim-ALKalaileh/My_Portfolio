b4db2a57197cfc46b101641d606278ce3306a40c
Ibrahim Kalaileh <ibrahim.al.kalaileh@gmail.com>
2023-02-12 00:42:10 +0300

    projects array

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,8 +1,41 @@
 import React from 'react'
 
 function projects() {
+  const projects =[
+    {
+        title: "Business Startup",
+        description: "Design & Development",
+        imgUrl: projImg1,
+      },
+      {
+        title: "Business Startup",
+        description: "Design & Development",
+        imgUrl: projImg2,
+      },
+      {
+        title: "Business Startup",
+        description: "Design & Development",
+        imgUrl: projImg3,
+      },
+      {
+        title: "Business Startup",
+        description: "Design & Development",
+        imgUrl: projImg1,
+      },
+      {
+        title: "Business Startup",
+        description: "Design & Development",
+        imgUrl: projImg2,
+      },
+      {
+        title: "Business Startup",
+        description: "Design & Development",
+        imgUrl: projImg3,
+      }
+  ]
+
   return (
-    <div>projects</div>
+    
   )
 }
 
